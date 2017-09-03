a146d34a37a6356f88f6c1efe6177744e9c80692
Boris32 <boris_rk@yahoo.com>
2017-09-03 14:27:07 +0200

    Absolute file path to relative

--- src/main/java/com/pages/DO_LoginScreen.java
+++ src/main/java/com/pages/DO_LoginScreen.java
@@ -34,7 +34,8 @@ public class DO_LoginScreen extends DO_BasePage {
 
     public void logInAsDefaultUser ()
     {
-        AppConfig apconfig = new AppConfig("C:\\Automation\\deleteThis\\local-app.properties");
+        //AppConfig apconfig = new AppConfig("C:\\Automation\\deleteThis\\local-app.properties");
+        AppConfig apconfig = new AppConfig (".\\local-app.properties");
         enterUsername(apconfig.readProperty("username"));
         enterPassword(apconfig.readProperty("password"));
         clickOnLogin();
